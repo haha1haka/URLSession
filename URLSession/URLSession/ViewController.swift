d1829ee72bebabc366c0f0b409629017e5fd98f0
haha1haka <haha1haka@icloud.com>
2022-11-09 17:43:02 +0900

    Third commit

--- URLSession/URLSession/ViewController.swift
+++ URLSession/URLSession/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        let urlStr = "https://github.com/haha1haka/URLSession/blob/main/Asset/스크린샷%2020221007%2023.32.36.png".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
-        guard let url = URL(string: "https://raw.githubusercontent.com/haha1haka/URLSession/main/Asset/Simulator Screen Recording-iPhone14-2022-10-06at 01.37.45.mp4") else {
+        guard let url = URL(string: "https://raw.githubusercontent.com/haha1haka/URLSession/main/Asset") else {
             print("이건가")
             return
         }
