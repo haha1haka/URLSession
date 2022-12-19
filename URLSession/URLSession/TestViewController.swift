75d77d1a7d172fe4a1e4aea8009c3a2f9b36c0b8
haha1haka <haha1haka@icloud.com>
2022-12-19 16:09:15 +0900

    코드 정리

--- URLSession/URLSession/TestViewController.swift
+++ URLSession/URLSession/TestViewController.swift
@@ -72,13 +72,3 @@ extension TestViewController: URLSessionDataDelegate {
 
     }
 }
-
-
-
-struct Lotto: Codable {
-    let totSellamnt: Int
-    let returnValue, drwNoDate: String
-    let firstWinamnt, drwtNo6, drwtNo4, firstPrzwnerCo: Int
-    let drwtNo5, bnusNo, firstAccumamnt, drwNo: Int
-    let drwtNo2, drwtNo3, drwtNo1: Int
-}
